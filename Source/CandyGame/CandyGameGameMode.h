f288b32ef387e0e784507849b298cebf6a9e65a6
Robin Lima <rlima.jr.2001@gmail.com>
2023-08-02 16:25:37 -0500

    Source

---
+++ Source/CandyGame/CandyGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CandyGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACandyGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACandyGameGameMode();
+};
+
+
+
